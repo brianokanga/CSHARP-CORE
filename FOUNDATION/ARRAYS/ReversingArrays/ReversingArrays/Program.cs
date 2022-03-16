84bf2e3e54aacf2379693460549ac94d1f5e902a
Brian <briannokanga@gmail.com>
2022-03-16 16:33:13 +0300

    Post review commit

--- FOUNDATION/ARRAYS/ReversingArrays/ReversingArrays/Program.cs
+++ FOUNDATION/ARRAYS/ReversingArrays/ReversingArrays/Program.cs
@@ -13,9 +13,35 @@ namespace ReversingArrays
             //Console.WriteLine(String.Join(',', arr));
 
             int[] array = { 9, 2, 5, 3, 7, 4, 8, 4 };
-            Array.Reverse(array);
 
-            Console.WriteLine(String.Join(',', array));
+            //Array.Reverse(array);
+            //Console.WriteLine(String.Join(',', array));
+
+            var results = ReverseArray(int[]array);
+            Console.WriteLine();
+
+
+            //int[] arr = { 1, 2, 3, 4 };
+            //var results = new int[] { };
+            //for(var i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    var results.push(arr[i]);
+            //}
+            //Console.WriteLine(arr1);
+
         }
+
+        public static void ReverseArray(int[] a)
+        {
+            if (a.Length == 0) return;
+            int n = a.Length - 1;
+            for (int i = 0; i <= n; i++)
+            {
+                int temp = a[n];
+                a[n--] = a[i];
+                a[i] = temp;
+            }
+        }
+
     }
 }
---
+++ FOUNDATION/ARRAYS/ReversingWords/ReversingWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReversingWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string s1 = "blue red green";
+            const string s2 = "c# rust python";
+
+            string rev1 = ReverseWords(s1);
+            Console.WriteLine(rev1);
+
+            string rev2 = ReverseWords(s2);
+            Console.WriteLine(rev2);
+        }
+
+        public static string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(" ");
+            Array.Reverse(words);
+            return string.Join(' ', words);
+        }
+    }
+}
